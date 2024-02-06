80d21367541b623a12dd85e6a4940cb648c71122
Giovanni Filippini <39963191+giovannifil-64@users.noreply.github.com>
2024-02-06 17:23:58 +0100

    Added favicon and dark mode updates

---
+++ style.js
@@ -0,0 +1,24 @@
+// Check if prefers-color-scheme is supported
+if (window.matchMedia('(prefers-color-scheme)').media !== 'not all') {
+    // Listen for theme changes
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+        const newColorScheme = e.matches ? 'dark' : 'light';
+        updateLogo(newColorScheme);
+    });
+}
+
+function updateLogo(colorScheme) {
+    const logo = document.getElementById('header-logo');
+    if (colorScheme === 'dark') {
+        logo.src = 'img/torchlight_dark.png';
+    } else {
+        logo.src = 'img/torchlight_light.png';
+    }
+}
+
+// Initially update logo based on the current color scheme
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    updateLogo('dark');
+} else {
+    updateLogo('light');
+}
